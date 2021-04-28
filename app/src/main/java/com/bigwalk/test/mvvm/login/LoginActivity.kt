5911041065ad23e523e402cf2a9a2136a843b358
seongjong <seongjong@ajk.co.kr>
2021-04-28 17:27:26 +0900

    feature - 로그인 액티비티 UI 수정 - 구글 파이어베이스 로그인 기능 구현

--- app/src/main/java/com/bigwalk/test/mvvm/login/LoginActivity.kt
+++ app/src/main/java/com/bigwalk/test/mvvm/login/LoginActivity.kt
@@ -5,8 +5,10 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import com.bigwalk.test.R
 import com.bigwalk.test.databinding.ActivityLoginBinding
+import com.bigwalk.test.mvvm.main.MainActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
@@ -15,7 +17,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class LoginActivity: AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     companion object {
         private const val TAG = "LoginActivity"
@@ -23,8 +25,6 @@ class LoginActivity: AppCompatActivity() {
     }
 
     private val viewModel: LoginViewModel by viewModel()
-    private lateinit var auth: FirebaseAuth
-    private lateinit var googleSignInClient: GoogleSignInClient
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,65 +42,42 @@ class LoginActivity: AppCompatActivity() {
         initEvent()
     }
 
-    fun initView(){
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.default_web_client_id))
-            .requestEmail()
-            .build()
+    fun initView() {
 
-        googleSignInClient = GoogleSignIn.getClient(this, gso)
-
-        auth = FirebaseAuth.getInstance()
     }
 
-    fun initEvent(){
-        signIn()
-    }
+    fun initEvent() {
+        with(viewModel) {
+            requestLogin.observe(this@LoginActivity, Observer {
+                it.getContentIfNotHandled().let {
+                    val signInIntent = googleSignInClient.signInIntent
+                    startActivityForResult(signInIntent, RC_SIGN_IN)
+                }
+            })
 
-    // [START on_start_check_user]
-    override fun onStart() {
-        super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser = auth.currentUser
+            moveToMainEvent.observe(this@LoginActivity, Observer {
+                it.getContentIfNotHandled().let {
+                    MainActivity.startActivity(this@LoginActivity)
+                    finish()
+                }
+            })
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
             try {
                 // Google Sign In was successful, authenticate with Firebase
                 val account = task.getResult(ApiException::class.java)!!
                 Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
-                firebaseAuthWithGoogle(account.idToken!!)
+                viewModel.firebaseAuthWithGoogle(account.idToken!!)
             } catch (e: ApiException) {
                 // Google Sign In failed, update UI appropriately
                 Log.w(TAG, "Google sign in failed", e)
             }
         }
-    }
-
-    private fun firebaseAuthWithGoogle(idToken: String) {
-        val credential = GoogleAuthProvider.getCredential(idToken, null)
-        auth.signInWithCredential(credential)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "signInWithCredential:success")
-                    val user = auth.currentUser
-
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "signInWithCredential:failure", task.exception)
-
-                }
-            }
-    }
-
-    private fun signIn() {
-        val signInIntent = googleSignInClient.signInIntent
-        startActivityForResult(signInIntent, RC_SIGN_IN)
     }
 }
--- app/src/main/java/com/bigwalk/test/mvvm/login/LoginViewModel.kt
+++ app/src/main/java/com/bigwalk/test/mvvm/login/LoginViewModel.kt
@@ -1,6 +1,52 @@
 package com.bigwalk.test.mvvm.login
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.bigwalk.test.R
+import com.bigwalk.test.util.Event
 
 class LoginViewModel : ViewModel() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+    val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        .requestIdToken(getString(R.string.default_web_client_id))
+        .requestEmail()
+        .build()
+
+    init {
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        auth = FirebaseAuth.getInstance()
+    }
+
+    val moveToMainEvent:MutableLiveData<Event<Unit>> = MutableLiveData()
+
+    val requestLogin:MutableLiveData<Event<Unit>> = MutableLiveData()
+
+    fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(LoginActivity.TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(LoginActivity.TAG, "signInWithCredential:failure", task.exception)
+
+                }
+            }
+    }
+
+    fun loginEvent(){
+        requestLogin.value = Event(Unit)
+    }
+
+    fun loginSuccessEvent(){
+        moveToMainEvent.value = Event(Unit)
+    }
 }
